d0af637d346ababdb3fae34788e0808d9f12dea5
Stefankow20p <91264106+Stefankow20p@users.noreply.github.com>
2021-11-15 23:56:12 +0100

    Update slideshow.js

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,13 +1,8 @@
 if (document.title == "Mapa") {
-    const parking = document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li")[5];
     const stolowka = document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li")[6];
-    const pralnia = document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li")[7];
-    const lodziarnia = document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li")[8];
     const pomost = document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li")[9];
     //5-9
-    const strona = "mapa";
 } else {
-    const strona = "rez";
     const lis = document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li");
     //0-16
 }
