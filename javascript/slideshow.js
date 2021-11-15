c140f5534343a8a77549cc2424b650d2532b10bd
Stefankow20p <91264106+Stefankow20p@users.noreply.github.com>
2021-11-16 00:17:11 +0100

    Update slideshow.js

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,8 +1,18 @@
 if (document.title == "Mapa") {
-    const stolowka = document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li")[6];
+    // const stolowka = {
+    //     path : document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li")[6],
+    //     slide_num : 3,
+    //     src1 : "img/mapa/stolowka/zdj1.jpg",
+    //     src2 : "img/mapa/stolowka/zdj2.jpg",
+    //     src3 : "img/mapa/stolowka/zdj3.jpg",
+    //     alt1 : ,
+    //     alt2 : ,
+    //     alt3 :
+    // }
     const pomost = document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li")[9];
     //5-9
 } else {
     const lis = document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li");
     //0-16
 }
+function change_slide() {}
