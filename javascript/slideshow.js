fd0e039e1a6f2a9504c130115b0ea47175ca26e5
Stefankow20p <91264106+Stefankow20p@users.noreply.github.com>
2021-11-16 13:42:17 +0100

    updated slideshow.js

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -10,10 +10,16 @@ if (document.title == "Mapa") {
     const stolowka = new Zdjecia(document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li")[6],
     ("img/mapa/stolowka/zdj1.jpg","img/mapa/stolowka/zdj2.jpg","img/mapa/stolowka/zdj3.jpg"),
     ("zdj1","zdj2","zdj3"))
-    const 
+    const pomost = new Zdjecia(document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li")[9],
+    ("img/mapa/pomost/zdj1.jpg","img/mapa/pomost/zdj2.jpg"),
+    ("zdj1","zdj2"))
     //5-9
 } else {
     const lis = document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li");
-    //0-16
+
+    const g1 = new Zdjecia(document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li")[0],
+    ("img/rezerwacjazdjecia/g1/zdj1.jpg","img/rezerwacjazdjecia/g1/zdj2.jpg","img/rezerwacjazdjecia/g1/zdj3.jpg"),
+    ("zdj1","zdj2","zdj3"))
+    //0-15
 }
 function change_slide() {}
