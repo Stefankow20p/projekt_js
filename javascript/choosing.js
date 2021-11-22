09e1dab7e672575faa95f2ba7b85b0c3f749aed4
Stefankow20p <91264106+Stefankow20p@users.noreply.github.com>
2021-11-22 21:42:21 +0100

    updated rezerwacje

--- javascript/choosing.js
+++ javascript/choosing.js
@@ -1,8 +1,54 @@
 const radios = document.querySelector("main").querySelector("section:nth-child(1)").querySelectorAll("input");
 const lis = document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li");
+const udogodnienia = document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll('input[type="checkbox"]');
+const liczba_os = [4, 2, 6, 4, 4, 2, 5, 8, 8, 2, 2, 2, 2, 2, 2, 2];
+const ceny = [170, 100, 300, 200, 190, 120, 300, 350, 350, 50, 50, 70, 70, 100, 150, 120];
+
+function wypisz_koszt(nr_pokoju) {
+    //ustala ilosc dni
+    console.log("start");
+    nr_pokoju = nr_pokoju - 1;
+    let date1 = document.querySelectorAll('input[type="date"]')[0].value.split("-");
+    console.log(date1);
+    date1 = new Date(date1[2], date1[0] - 1, date1[1]);
+    let date2 = document.querySelectorAll('input[type="date"]')[1].value.split("-");
+    console.log(date2);
+    date2 = new Date(date2[2], date2[0] - 1, date2[1]);
+    console.log(date1);
+    console.log(date2);
+    const ilosc_dni = Math.round((date2 - date1) / (1000 * 60 * 60 * 24 * 365));
+    // sprawdza dodatkowe udogodnienia
+    let mnoznik = 0;
+    if (udogodnienia[0].checked) {
+        mnoznik += 20;
+    }
+    if (udogodnienia[1].checked) {
+        mnoznik += 50;
+    }
+    if (udogodnienia[2].checked) {
+        mnoznik += 10;
+    }
+    if (udogodnienia[3].checked) {
+        mnoznik += 15 * liczba_os[nr_pokoju];
+    }
+    if (udogodnienia[4].checked) {
+        mnoznik += 25 * liczba_os[nr_pokoju];
+    }
+    console.log(mnoznik);
+    console.log(nr_pokoju);
+    console.log(ilosc_dni);
+
+    document.querySelector("main").querySelector("form").querySelectorAll("li")[1].innerHTML = mnoznik * ilosc_dni + ceny[nr_pokoju] * ilosc_dni;
+    console.log("f");
+}
+
 function change_value(item) {
     const inp = document.querySelector("main").querySelector("section:nth-child(2)").querySelector('input[type="hidden"]');
     inp.value = item.slice(3, 5);
+    document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("ul")[1].querySelectorAll("li")[1].innerHTML =
+        ceny[item.slice(3, 5) - 1];
+    document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("ul")[2].querySelectorAll("li")[1].innerHTML =
+        liczba_os[item.slice(3, 5) - 1];
     document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("li")[16].innerHTML = "Nr: " + item.slice(3, 5);
     for (i = 0; i < 16; i++) {
         lis[i].style.display = "none";
@@ -12,6 +58,7 @@ function change_value(item) {
 for (i = 0; i < radios.length; i++) {
     radios[i].addEventListener("change", function () {
         change_value(this.id);
+        wypisz_koszt(document.querySelector("main").querySelector("section:nth-child(2)").querySelector('input[type="hidden"]').value);
     });
 }
 
@@ -21,3 +68,18 @@ for (i = 0; i < 16; i++) {
 }
 lis[0].style.display = "block";
 lis[16].innerHTML = "Nr: 1";
+
+document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("ul")[1].querySelectorAll("li")[1].innerHTML = "170";
+document.querySelector("main").querySelector("section:nth-child(2)").querySelectorAll("ul")[2].querySelectorAll("li")[1].innerHTML = "4";
+
+for (i = 0; i < udogodnienia.length; i++) {
+    udogodnienia[i].addEventListener("change", function () {
+        wypisz_koszt(document.querySelector("main").querySelector("section:nth-child(2)").querySelector('input[type="hidden"]').value);
+    });
+}
+document.querySelectorAll('input[type="date"]')[0].addEventListener("change", function () {
+    wypisz_koszt(document.querySelector("main").querySelector("section:nth-child(2)").querySelector('input[type="hidden"]').value);
+});
+document.querySelectorAll('input[type="date"]')[1].addEventListener("change", function () {
+    wypisz_koszt(document.querySelector("main").querySelector("section:nth-child(2)").querySelector('input[type="hidden"]').value);
+});
