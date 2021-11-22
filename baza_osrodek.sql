2414c8bf9f2e423ff98405f2061df2929bea9e7a
Stefankow20p <91264106+Stefankow20p@users.noreply.github.com>
2021-11-22 20:24:12 +0100

    Update baza_osrodek.sql

--- baza_osrodek.sql
+++ baza_osrodek.sql
@@ -7,6 +7,7 @@ use baza_osrodek;
 CREATE TABLE pokoje(
 		id_pokoj int,
 		ilosc_osob int,
+		cena int,
 		PRIMARY KEY (id_pokoj)
 		);
 
@@ -38,22 +39,22 @@ CREATE TABLE wynajecia(
 
 
 
-INSERT INTO pokoje values (1,"4");
-INSERT INTO pokoje values (2,"2");
-INSERT INTO pokoje values (3,"6");
-INSERT INTO pokoje values (4,"4");
-INSERT INTO pokoje values (5,"4");
-INSERT INTO pokoje values (6,"2");
-INSERT INTO pokoje values (7,"5");
-INSERT INTO pokoje values (8,"8");
-INSERT INTO pokoje values (9,"8");
-INSERT INTO pokoje values (10,"2");
-INSERT INTO pokoje values (11,"2");
-INSERT INTO pokoje values (12,"2");
-INSERT INTO pokoje values (13,"2");
-INSERT INTO pokoje values (14,"2");
-INSERT INTO pokoje values (15,"2");
-INSERT INTO pokoje values (16,"2");
+INSERT INTO pokoje values (1,4,170);
+INSERT INTO pokoje values (2,2,100);
+INSERT INTO pokoje values (3,6,300);
+INSERT INTO pokoje values (4,4,200);
+INSERT INTO pokoje values (5,4,190);
+INSERT INTO pokoje values (6,2,120);
+INSERT INTO pokoje values (7,5,300);
+INSERT INTO pokoje values (8,8,350);
+INSERT INTO pokoje values (9,8,350);
+INSERT INTO pokoje values (10,2,50);
+INSERT INTO pokoje values (11,2,50);
+INSERT INTO pokoje values (12,2,70);
+INSERT INTO pokoje values (13,2,70);
+INSERT INTO pokoje values (14,2,100);
+INSERT INTO pokoje values (15,2,150);
+INSERT INTO pokoje values (16,2,120);
 
 INSERT INTO znizki(dzien, wartosc) values ("2021-10-16",0.6);
 INSERT INTO znizki(dzien, wartosc) values ("2021-10-17",0.6);
