3cfce04c9371f46737bac69a67739cfe1b94c00c
Stefankow20p <91264106+Stefankow20p@users.noreply.github.com>
2021-11-24 14:49:18 +0100

    set up basic connection between server and page

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening at port ${port}`));
 app.use(express.static("public"));
 app.use(express.json());
+
+app.post("/api", (req, res) => {
+    console.log("I got a request");
+    console.log(req.body);
+});
