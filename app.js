2f705fd9565a641fd2d5e0cffe8d8f27d784080a
Stefankow20p <91264106+Stefankow20p@users.noreply.github.com>
2021-11-24 14:43:30 +0100

    set up

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("server runs");
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening at port ${port}`));
+app.use(express.static("public"));
+app.use(express.json());
